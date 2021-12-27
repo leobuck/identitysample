2f2cbf0a99b1619a16106c5bc1684930cd4dfcd2
leobuck <leobuck12@gmail.com>
2021-12-27 11:52:51 -0300

    Customizando o Identity

---
+++ IdentitySample/App_Data/script.sql
@@ -0,0 +1,5 @@
+insert into AspNetRoles values (NEWID(), 'Admin');
+insert into AspNetRoles values (NEWID(), 'User');
+
+insert into AspNetUserRoles values ('57718b5a-f20c-4407-b4e7-98348a03593a', 'B58EB6CF-19AE-4735-83A7-83EB8EF9E61E')
+insert into AspNetUserRoles values ('a85519cd-9a1b-47ab-8814-31c4e040c886', 'B1B8D6E7-698C-40E4-9B0D-0CDAB4A9E0CA')
